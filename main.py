1676ee8038de15d8cfaff9646aa44129554e7ec0
User0895 <94646472+User0895@users.noreply.github.com>
2021-11-22 22:19:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,33 +14,51 @@ loco_drone.connect()
 loco_drone.drone_calibrate()
 loco_drone.drone_takeoff()
 
+# Determines what mode of operation the user would like then turns to that.
+def invoke(input):
+    if input.lower() == "tilt":
+        loco_drone.set_mode(loco_drone.MODE_ACCELEROMETER)
+        break
+    elif input.lower() == "control":
+        loco_drone.set_mode(loco_drone.MODE_CONTROL)
+        break
+    elif input.lower() == "joystick":
+        loco_drone.set_mode(loco_drone.MODE_JOYSTICK)
+        break
+    else:
+        print("Invalid inpput.")
+        continue
+    
 #DEFINITIONS--------------------------------------------------------------------------------
 
 # Hover
-def hover(value, wait):
-    # Should X be zero?
-    loco_drone.set_data(0, value, 0)  
+def hover(wait=5):
+    loco_drone.set_data(0, 0, 0)  
     time.sleep(wait) 
 
-# Vertical - Z value.
-def vertical(value, wait):
+# Vertical (U/D) - Z value.
+def vertical(value, wait=5):
     loco_drone.set_data(0, 0, value)  
     time.sleep(wait)
+    loco_drone.set_data(0, 0, 0)
 
-# Roll - X value.
-def roll_right(value, wait):
+# Roll (L/R) - X value.
+def roll_right(value, wait=5):
     loco_drone.set_data(value, 0, 0)
     time.sleep(wait)
+    loco_drone.set_data(0, 0, 0)
 
 # Pitch (B/F) - Y value.
-def pitch_1(value, wait):
+def pitch_bf(value, wait=5):
     loco_drone.set_data(0, value, 0)
     time.sleep(wait)
+    loco_drone.set_data(0, 0, 0)
 
 # Roll/Pitch (L/R) - X, Y value.
-def pitch_2(value1, value2, wait):
+def pitch_lr(value1, value2, wait=5):
     loco_drone.set_data(value1, value2, 0)
     time.sleep(wait)
+    loco_drone.set_data(0, 0, 0)
 
 # Flip drone.
 def flip(input):
@@ -55,27 +73,12 @@ def flip(input):
             print("Invlaid input.")
             continue
     
-# Forward
-def go_forward(value, wait):
-    loco_drone.set_data(0, value, 0)
-    time.sleep(wait)
-    
-#JOYSTICK----------------------------------------------------------------------------------
-
-#Start Joystick Controls
-"""
-loco_drone.set_mode(loco_drone.MODE_JOYSTICK)
-time.sleep(300)
-loco_drone.set_mode(loco_drone.MODE_CONTROL)
-"""
-
 
 #EXECUTION---------------------------------------------------------------------------------
 
-vertical(2, 1)
-hover(0, 1)
-go_forward(3, 1)
-vertical(-2, 1)
+vertical(1)
+pitch_bf(25)
+hover()
 
 #END---------------------------------------------------------------------------------------
 
